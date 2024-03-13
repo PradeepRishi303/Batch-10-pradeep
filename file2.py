e97b33487c4f939cc13fae90c596c66170c838ff
PradeepRishi303 <162984104+PradeepRishi303@users.noreply.github.com>
2024-03-13 09:23:04 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,19 @@
+
+
+#print("hello world")
+
+import this
+print(this)
+import file 1
+
+,,,
+Bargained characters
+1. Controls and Data structures -> ABC language
+2. Strings and its formats -> from C
+3. Regular expressions -> Pearl
+4. Excep[tion handling -> Module 3
+5. Magic methods from -> fortron
+         
+,,,         
+
+class anthmatic:
